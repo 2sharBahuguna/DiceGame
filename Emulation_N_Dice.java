6aea74757cc11081b4aeed3b767bb58ee9dea733
Tushar Bahuguna <111565432+2sharBahuguna@users.noreply.github.com>
2022-09-24 11:52:32 +0530

    Create Emulation_N_Dice.java

---
+++ Emulation_N_Dice.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+
+ 
+
+public class Emulation_N_Dice {
+    public static void main(String args[])
+    {
+
+        System.out.println("Enter the number of dice: ");
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Random rand = new Random();
